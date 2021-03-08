0b0ae1065fdb40ec88491e04455a4043d69fb9d9
Sören Gürtler <soeren@guertler.net>
2021-03-08 12:55:02 +0100

    parse cli options

--- src/main/java/crawler/Main.java
+++ src/main/java/crawler/Main.java
@@ -1,12 +1,47 @@
 package crawler;
 
+import org.apache.commons.cli.*;
+
 public class Main {
-    public static void main(String[] args) {
-        String rootUrl = args[args.length - 1];
+    private static String rootUrl;
+    private static int maxDepth;
+    private static boolean omitDuplicates;
+
+    public static void main(String[] args) throws Exception {
+        Options cliOptions = getCliOptions();
+        CommandLine cmd = new DefaultParser().parse(cliOptions, args);
+
+        if(cmd.hasOption("help") || !cmd.hasOption("url")) {
+            new HelpFormatter().printHelp("Webcrawler", cliOptions, true);
+            System.exit(0);
+        }
+
+        parseCliOptions(cmd);
+    }
+
+    private static void parseCliOptions(CommandLine cmd) {
+        rootUrl = cmd.getOptionValue("url");
         if(!isValidHttpUrl(rootUrl)) {
             System.err.printf("\"%s\" is not a valid Http URL!", rootUrl);
             System.exit(1);
         }
+
+        maxDepth = Integer.parseInt(cmd.getOptionValue("max-depth", "2"));
+        if(maxDepth < 1 || maxDepth > 50) {
+            System.err.printf("%d is not a valid search depth", maxDepth);
+            System.exit(1);
+        }
+
+        omitDuplicates = Boolean.parseBoolean(cmd.getOptionValue("omit-duplicates", "false"));
+    }
+
+    private static Options getCliOptions() {
+        Options options = new Options();
+        options.addOption("d","max-depth",true, "Specify the recursion depth for following links [1-50]");
+        options.addOption("o","omit-duplicates",true, "Omit duplicate pages");
+        options.addOption("u", "url", true, "Specify the root url for the crawler");
+        options.addOption("h", "help", false, "Open the help dialog");
+        return options;
     }
 
     public static boolean isValidHttpUrl(String url) {
