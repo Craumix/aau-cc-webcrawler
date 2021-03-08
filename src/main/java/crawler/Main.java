018767759509daea8987aef48dfb3730435521e2
Sören Gürtler <soeren@guertler.net>
2021-03-08 12:31:33 +0100

    include apache commons cli

--- src/main/java/crawler/Main.java
+++ src/main/java/crawler/Main.java
@@ -10,7 +10,6 @@ public class Main {
     }
 
     public static boolean isValidHttpUrl(String url) {
-        //https://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url
         return url.matches("https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)");
     }
 }
