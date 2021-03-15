6b1d277282d9ebe51ab38ef8ca464f437ad03673
Sören Gürtler <soeren@guertler.net>
2021-03-15 23:48:35 +0100

    put params default values in constants

--- src/main/java/crawler/Main.java
+++ src/main/java/crawler/Main.java
@@ -8,6 +8,8 @@ import java.io.PrintStream;
 import java.util.concurrent.*;
 
 public class Main {
+    private static final int DEFAULT_MAX_DEPTH = 2, DEFAULT_THREAD_COUNT = 2, DEFAULT_MAX_LINKS_PER_PAGE = 100;
+
     private static String rootUrl, outputFile;
     private static int maxDepth, threadCount, maxLinksPerPage;
     private static boolean omitDuplicates;
@@ -54,19 +56,19 @@ public class Main {
             System.exit(1);
         }
 
-        maxDepth = Integer.parseInt(cmd.getOptionValue("max-depth", "2"));
+        maxDepth = Integer.parseInt(cmd.getOptionValue("max-depth", DEFAULT_MAX_DEPTH + ""));
         if(maxDepth < 1 || maxDepth > 10) {
             System.err.printf("%d is not a valid search depth", maxDepth);
             System.exit(1);
         }
 
-        threadCount = Integer.parseInt(cmd.getOptionValue("threads", "2"));
+        threadCount = Integer.parseInt(cmd.getOptionValue("threads", DEFAULT_THREAD_COUNT + ""));
         if(threadCount < 1 || threadCount > 1024) {
             System.err.printf("%d is not a valid Thread count", threadCount);
             System.exit(1);
         }
 
-        maxLinksPerPage = Integer.parseInt(cmd.getOptionValue("max-links", "1000"));
+        maxLinksPerPage = Integer.parseInt(cmd.getOptionValue("max-links", DEFAULT_MAX_LINKS_PER_PAGE + ""));
         if(maxLinksPerPage < 1) {
             System.err.printf("Max links to follow should be > 1");
             System.exit(1);
@@ -78,12 +80,12 @@ public class Main {
 
     private static Options getCliOptions() {
         Options options = new Options();
-        options.addOption("d","max-depth",true, "Specify the recursion depth for following links. Default: 2, Range 1-10");
+        options.addOption("d","max-depth",true, String.format("Specify the recursion depth for following links. Default: %d, Range 1-10", DEFAULT_MAX_DEPTH));
         options.addOption("s","omit-duplicates",false, "If set omits duplicate pages");
         options.addOption("u", "url", true, "Specify the root url for the crawler");
         options.addOption("o", "output", true, "Output file can be specified as alternative to stdout");
-        options.addOption("t", "threads", true, "How many threads to use, will increase CPU and Memory consumption. Default: 2, Range 1-1024");
-        options.addOption("l", "max-links", true, "Max links to follow per page. Default: 1000, Range: 1-2147483647");
+        options.addOption("t", "threads", true, String.format("How many threads to use, will increase CPU and Memory consumption. Default: %d, Range 1-1024", DEFAULT_THREAD_COUNT));
+        options.addOption("l", "max-links", true, String.format("Max links to follow per page. Default: %d, Range: 1-inf", DEFAULT_MAX_LINKS_PER_PAGE));
         options.addOption("h", "help", false, "Open the help dialog");
         return options;
     }
