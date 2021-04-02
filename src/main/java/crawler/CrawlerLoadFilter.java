2bc6593c93f3a4378198fa7c7649e9f78d31a256
Sören Gürtler <soeren@guertler.net>
2021-04-02 15:02:34 +0200

    add better duplicate detection and test

--- src/main/java/crawler/CrawlerLoadFilter.java
+++ src/main/java/crawler/CrawlerLoadFilter.java
@@ -10,7 +10,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 public class CrawlerLoadFilter implements WebpageLoadFilter {
 
-    private static final CopyOnWriteArrayList<URI> uriLog = new CopyOnWriteArrayList<>();
+    private static final CopyOnWriteArrayList<String> urlLog = new CopyOnWriteArrayList<>();
     private static final ConcurrentHashMap<String, RobotsTxt> hostRobotsMap = new ConcurrentHashMap<>();
 
     private final boolean omitDuplicates, useRobotsTxt;
@@ -25,18 +25,29 @@ public class CrawlerLoadFilter implements WebpageLoadFilter {
         if(!Util.isValidHttpUrl(uri))
             return false;
 
-        if(omitDuplicates) {
-            if(uriLog.contains(uri)) {
+        if(omitDuplicates)
+            if(urlLog.contains(asCleanUriString(uri)))
                 return false;
-            }else {
-                uriLog.add(uri);
-            }
-        }
+            else
+                urlLog.add(asCleanUriString(uri));
 
         if(useRobotsTxt && !allowedByRobotsTxt(uri))
             return false;
 
         return true;
+    }
+
+    private String asCleanUriString(URI uri) {
+        //Strip URI Scheme
+        String url = uri.toString().replace(uri.getScheme() + "://", "");
+
+        //Remove trailing "#", "/", or "/#"
+        if(url.endsWith("#"))
+            url = url.substring(0,  url.length() - 1);
+        if(url.endsWith("/"))
+            url = url.substring(0,  url.length() - 1);
+
+        return url;
     }
 
     private boolean allowedByRobotsTxt(URI uri) {
--- src/test/java/FilterTest.java
+++ src/test/java/FilterTest.java
@@ -1,11 +1,23 @@
 import crawler.CrawlerLoadFilter;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class FilterTest {
 
     @Test
-    void testDuplicateFilter() {
+    void testDuplicateFilter() throws URISyntaxException {
         CrawlerLoadFilter filter = new CrawlerLoadFilter(true, false);
+
+        assertTrue(filter.webpageShouldBeLoaded(new URI("https://google.com")));
+
+        assertFalse(filter.webpageShouldBeLoaded(new URI("http://google.com")));
+
+        assertFalse(filter.webpageShouldBeLoaded(new URI("https://google.com/")));
+        assertFalse(filter.webpageShouldBeLoaded(new URI("https://google.com#")));
+        assertFalse(filter.webpageShouldBeLoaded(new URI("https://google.com/#")));
     }
 }
