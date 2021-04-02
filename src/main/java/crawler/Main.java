076896792e752570440ee9b5b0ca24349c1d0330
Merlin Volkmer <merlin.volkmer@outlook.com>
2021-04-02 13:34:56 +0200

    fixed user agent

--- src/main/java/crawler/Main.java
+++ src/main/java/crawler/Main.java
@@ -14,7 +14,6 @@ public class Main {
             MAX_THREAD_COUNT = 1024,
             DEFAULT_MAX_LINKS_PER_PAGE = 100;
     private static final String
-            DEFAULT_USER_AGENT = "AAU CleanCode WebCrawler (https://github.com/Craumix/aau-cc-webcrawler)",
             BROWSER_USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0";
 
 
@@ -33,7 +32,8 @@ public class Main {
         if(!parseCliOptions(cmd))
             System.exit(1);
 
-        Webpage.setRequestUserAgent(fakeBrowser ? BROWSER_USER_AGENT : DEFAULT_USER_AGENT);
+        if (fakeBrowser)
+            Webpage.setRequestUserAgent(BROWSER_USER_AGENT);
         Webpage.setMaxChildrenPerPage(maxLinksPerPage);
 
         CrawlerLoadFilter loadFilter = new CrawlerLoadFilter(omitDuplicates, useRobotsTxt);
