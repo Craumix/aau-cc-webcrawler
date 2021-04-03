fd75cf267da95d46a8b189dc3c29d847f0d0cf98
Sören Gürtler <soeren@guertler.net>
2021-04-03 11:54:02 +0200

    add more filer test

--- src/test/java/FilterTest.java
+++ src/test/java/FilterTest.java
@@ -17,9 +17,31 @@ public class FilterTest {
         assertTrue(filter.webpageShouldBeLoaded(new URI("https://google.com")));
 
         assertFalse(filter.webpageShouldBeLoaded(new URI("http://google.com")));
-
         assertFalse(filter.webpageShouldBeLoaded(new URI("https://google.com/")));
         assertFalse(filter.webpageShouldBeLoaded(new URI("https://google.com#")));
         assertFalse(filter.webpageShouldBeLoaded(new URI("https://google.com/#")));
+    }
+
+    @Test
+    @DisplayName("Test CrawlerLoadFilter for invalid urls")
+    void testFilterInvalidUrlDetection() throws URISyntaxException {
+        CrawlerLoadFilter filter = new CrawlerLoadFilter(false, false);
+
+        assertTrue(filter.webpageShouldBeLoaded(new URI("https://google.com")));
+
+        assertFalse(filter.webpageShouldBeLoaded(new URI("abc://domain.com")));
+        assertFalse(filter.webpageShouldBeLoaded(new URI("http://domain")));
+    }
+
+    @Test
+    @DisplayName("Test RobotsTxt")
+    void testRobotsTxt() throws URISyntaxException {
+        CrawlerLoadFilter filter = new CrawlerLoadFilter(false, true);
+
+        assertTrue(filter.webpageShouldBeLoaded(new URI("https://crawler-test.com")));
+        assertTrue(filter.webpageShouldBeLoaded(new URI("https://crawler-test.com/robots_protocol/page_allowed_with_robots")));
+
+        assertFalse(filter.webpageShouldBeLoaded(new URI("https://crawler-test.com/infinite")));
+        assertFalse(filter.webpageShouldBeLoaded(new URI("https://crawler-test.com/speed_test")));
     }
 }
