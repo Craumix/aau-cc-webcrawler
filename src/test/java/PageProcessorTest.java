3507b619427a280b1451cd94a4ffc0914c9c741f
Merlin Volkmer <merlin.volkmer@outlook.com>
2021-05-14 17:03:54 +0200

    Change the PageProcessorTest to use the mocked fetcher

--- src/test/java/PageProcessorTest.java
+++ src/test/java/PageProcessorTest.java
@@ -1,3 +1,4 @@
+import connector.LocalFileFetcher;
 import crawler.webpage.AsyncWebpageLoader;
 import crawler.webpage.Webpage;
 import org.junit.jupiter.api.DisplayName;
@@ -10,13 +11,14 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PageProcessorTest {
 
     @Test
-    @DisplayName("Tests the AsyncPageProcessor by loading google.com with a depth of 2")
+    @DisplayName("Tests the AsyncPageProcessor by loading a site with a depth of 2")
     void testPageProcessorWithValidInputs() throws URISyntaxException, InterruptedException {
-        Webpage rootPage = new Webpage("https://google.com");
+        Webpage.setConnector(new LocalFileFetcher());
+        Webpage rootPage = new Webpage("3-children");
         AsyncWebpageLoader webpageProcessor = new AsyncWebpageLoader(rootPage, 2, 1);
         webpageProcessor.loadPagesRecursively();
 
-        assertFalse(rootPage.getPageTitle().equals(""));
-        assertFalse(rootPage.getChildren().get(0).getPageTitle().equals(""));
+        assertNotEquals("", rootPage.getPageTitle());
+        assertNotEquals("", rootPage.getChildren().get(0).getPageTitle());
     }
 }
